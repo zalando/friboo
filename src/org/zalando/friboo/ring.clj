1f0b0f96b2db854983976254a1ee274e4f14867a
Tobias Sarnowski <tobias@sarnowski.io>
2015-03-18 23:58:03 +0100

    added some ring helpers

---
+++ src/org/zalando/friboo/ring.clj
@@ -0,0 +1,16 @@
+(ns org.zalando.friboo.ring
+  (:require [ring.util.response :refer :all]))
+
+;; some convinience helpers
+
+(defn content-type-json
+  "Sets the content-type of the response to 'application/json'."
+  [response]
+  (content-type response "application/json"))
+
+(defn single-response
+  "Returns 404 if results is empty or the first result, ignoring all others."
+  [results]
+  (if (empty? results)
+    (not-found {})
+    (response (first results))))
