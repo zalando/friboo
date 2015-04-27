c917fa6ba6bc14df45d90b0fed87c4525a35981a
Andre Hartmann <Andre.Hartmann@zalando.de>
2015-04-27 12:39:53 +0200

    removed hard-coded gpg key

--- project.clj
+++ project.clj
@@ -40,6 +40,5 @@
 
   :aliases {"cloverage" ["with-profile" "test" "cloverage"]}
 
-  :signing {:gpg-key "tobias.sarnowski@zalando.de"}
   :deploy-repositories {"releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/" :creds :gpg}
                         "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/" :creds :gpg}})
