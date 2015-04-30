0363ad18a3496694806c96c7f46b52b849191e74
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-04-30 10:56:43 +0200

    adopted friboo to check for user.json and client.json

--- src/org/zalando/stups/friboo/system/credentials.clj
+++ src/org/zalando/stups/friboo/system/credentials.clj
@@ -17,35 +17,39 @@
 (extend File json/JSONWriter
   {:-write serialize-file})
 
-
 (defn- check-file
   "Checks a file if it has new credentials and updates the credentials atom."
-  [file credentials]
+  [type file credentials]
   (try
-    (let [content (slurp file)
-          data (json/read-str content)]
-      (when (and data (not (empty? data)) (not= data @credentials)
-        (reset! credentials data)
-        (log/info "New credentials found for %s." (select-keys data ["application_username" "client_id"])))))
+    (let [data (-> file slurp json/read-str)]
+      (when (and (not-empty data) (not= data (get @credentials type)))
+        (swap! credentials assoc type data)
+        (log/info "New credentials for %s in %s found." type file)))
     (catch Exception e
-      (log/warn "Cannot read credentials file %s because %s." file (str e)))))
+      (log/warn "Cannot read %s credentials file %s because %s." type file (.getMessage e)))))
 
-; configuration map has to contain :file key
+; configuration map has to contain :dir key
 ; credentials key will be set during initilization and holds atom with credentials
 (defrecord CredentialUpdater [configuration credentials]
   component/Lifecycle
 
   (start [this]
-    (let [^File file (io/file (require-config configuration :file))
-          credentials (atom nil)]
-      (log/info "Watching %s for credentials." file)
-      (check-file file credentials)
-      (start-watch [{:path        (.getParent file)
-                     :event-types [:create :modify]
-                     :callback    (fn [_ filename]
-                                    (when (= filename (str file))
-                                      (check-file file credentials)))}])
-      (assoc this :credentials credentials)))
+    (if-let [dir (:dir configuration)]
+      (let [credentials (atom {})
+            dir (.getAbsolutePath (io/file dir))]
+        (doseq [[type file] {:user   "user.json"
+                             :client "client.json"}]
+          (swap! credentials assoc type nil)
+          (let [source (io/file dir file)]
+            (log/info "Watching %s for %s credentials." source type)
+            (start-watch [{:path        (.getParent source)
+                           :event-types [:create :modify]
+                           :callback    (fn [_ filename]
+                                          (when (= filename (str source))
+                                            (check-file type source credentials)))}])
+            (check-file type source credentials)))
+        (assoc this :credentials credentials))
+      (log/warn "No credentials directory given, will not watch for any credentials.")))
 
   (stop [this]
     ; unfortunatly, start-watch does not provide stop-watch
