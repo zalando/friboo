c420dae7c63a7e739957aa3add91011c458bd3c7
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-05-13 16:27:26 +0200

    log denies

--- src/org/zalando/stups/friboo/user.clj
+++ src/org/zalando/stups/friboo/user.clj
@@ -2,7 +2,8 @@
   (:require [clj-http.client :as http]
             [org.zalando.stups.friboo.ring :as r]
             [io.sarnowski.swagger1st.util.api :as api]
-            [org.zalando.stups.friboo.config :refer [require-config]]))
+            [org.zalando.stups.friboo.config :refer [require-config]]
+            [org.zalando.stups.friboo.log :as log]))
 
 (defn require-teams
   "Returns a set of teams, a user is part of or throws an exception if user is in no team."
@@ -12,14 +13,17 @@
    (require-teams (get tokeninfo "uid") (get tokeninfo "access_token") team-service-url))
   ([user-id token team-service-url]
    (when-not user-id
+     (log/warn "ACCESS DENIED (unauthenticated) because token does not contain user information.")
      (api/throw-error 403 "no user information available"))
    (let [response (http/get (r/conpath team-service-url "/user/" user-id)
                             {:oauth-token token
                              :as          :json})
          teams (:body response)]
      (if (empty? teams)
-       (api/throw-error 403 "user has no teams"
-                        {:user user-id})
+       (do
+         (log/warn "ACCESS DENIED (unauthorized) because user is not any team.")
+         (api/throw-error 403 "user has no teams"
+                          {:user user-id}))
        (into #{} (map :id teams))))))
 
 (defn require-team
@@ -31,6 +35,7 @@
   ([team user-id token team-service-url]
    (let [in-team? (require-teams user-id token team-service-url)]
      (when-not (in-team? team)
+       (log/warn "ACCESS DENIED (unauthorized) because user is not in team %s." team)
        (api/throw-error 403 (str "user not in team '" team "'")
                         {:user          user-id
                          :required-team team
