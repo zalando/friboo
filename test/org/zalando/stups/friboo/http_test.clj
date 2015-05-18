5409aaca8a02f6a9577aaa2fe664cf36064661b1
Henning Jacobs <henning@jacobs1.de>
2015-05-18 14:44:36 +0200

    test basic auth

--- test/org/zalando/stups/friboo/http_test.clj
+++ test/org/zalando/stups/friboo/http_test.clj
@@ -3,7 +3,10 @@
     [clojure.test :refer :all]
     [org.zalando.stups.friboo.system.http :refer :all]))
 
-(deftest test-map-authorization-header
+(deftest test-map-authorization-header-simple
   (is (= "xyz" (map-authorization-header "xyz")))
   (is (= "Bearer 123" (map-authorization-header "Token 123"))))
+
+(deftest test-map-authorization-header-basic-auth
+  (is (= "Bearer 123" (map-authorization-header "Basic b2F1dGgyOjEyMw=="))))
 
