5c10b2608453b2b1e2112d20a1c647b8a3055cab
Tobias Sarnowski <tobias@sarnowski.io>
2015-05-22 20:20:24 +0200

    upgraded tokens

--- project.clj
+++ project.clj
@@ -28,7 +28,7 @@
                  [amazonica "0.3.22" :exclusions [org.apache.httpcomponents/httpclient joda-time]]
                  [org.clojure/data.codec "0.1.0"]
                  [overtone/at-at "1.2.0"]
-                 [org.zalando.stups/tokens "0.4.0"]
+                 [org.zalando.stups/tokens "0.5.0"]
                  [com.netflix.hystrix/hystrix-clj "1.4.8"]
                  [com.netflix.hystrix/hystrix-core "1.4.8"]
                  [com.netflix.hystrix/hystrix-metrics-event-stream "1.4.8"]]
