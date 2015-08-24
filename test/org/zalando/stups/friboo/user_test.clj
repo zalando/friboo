c41b5d679b86c0f0b4e269dcba4b7e83c4c933df
Andre Hartmann <Andre.Hartmann@zalando.de>
2015-08-24 15:03:36 +0200

    get "teams" from aws accounts

--- test/org/zalando/stups/friboo/user_test.clj
+++ test/org/zalando/stups/friboo/user_test.clj
@@ -1,7 +1,9 @@
 (ns org.zalando.stups.friboo.user-test
   (:require
     [clojure.test :refer :all]
-    [org.zalando.stups.friboo.user :refer :all])
+    [org.zalando.stups.friboo.user :refer :all]
+    [org.zalando.stups.friboo.test-utils :refer :all]
+    [clj-http.client :as http])
   (:import (clojure.lang ExceptionInfo)))
 
 (deftest test-trim-slashes
@@ -32,3 +34,14 @@
 (deftest test-require-missing-realm
   (let [ex (is (thrown? ExceptionInfo (require-realms #{"services"} {:tokeninfo {"realm" "employees"}})))]
     (is (= (-> ex .getData :http-code) 403))))
+
+(deftest test-get-teams
+  (let [calls (atom [])]
+    (with-redefs [http/get (comp (constantly {:body [{:name "team1"} {:name "team2"}]})
+                                 (track calls :http-get))]
+      (is (= (get-teams "https://teams.example.org" "TOKEN12345" "a-user") [{:name "team1"} {:name "team2"}]))
+      (is (= (count @calls) 1))
+      (is (= (:args (first @calls))
+             ["https://teams.example.org/api/accounts/aws" {:query-params {:member "a-user"}
+                                                            :oauth-token "TOKEN12345"
+                                                            :as :json}])))))
