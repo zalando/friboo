4431bae206e0cc6ca650d5a3fe24e0eb8349cb07
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-08-28 10:21:13 +0200

    Fixes conpath bug

--- src/org/zalando/stups/friboo/ring.clj
+++ src/org/zalando/stups/friboo/ring.clj
@@ -29,15 +29,27 @@
     (not-found {})
     (response (first results))))
 
+(defmacro xor
+  [a b]
+  `(or (and ~a (not ~b))
+       (and (not ~a) ~b)))
+
 (defn conpath
   "Concatenates path elements to an URL."
   [url & path]
   (let [[x & xs] path]
     (if x
-      (let [url (if (or
-                      (.endsWith url "/")
-                      (.startsWith x "/"))
+      (let [first-with-slash (.endsWith url "/")
+            last-with-slash  (.startsWith x "/")
+            url (if (xor first-with-slash
+                         last-with-slash)
+                  ; concat if exactly one of them has a /
                   (str url x)
-                  (str url "/" x))]
+                  (if (and first-with-slash
+                           last-with-slash)
+                    ; if both have a /, remove one
+                    (str url (s/replace-first x #"/" ""))
+                    ; if none have a /, add one
+                    (str url "/" x)))]
         (recur url xs))
       url)))
