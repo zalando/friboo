8306065ea477ad0789f59630a829f3ab56b5fd80
Andre Hartmann <Andre.Hartmann@zalando.de>
2015-08-28 18:02:43 +0200

    Version 1.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/friboo "1.1.1-SNAPSHOT"
+(defproject org.zalando.stups/friboo "1.1.1"
   :description "A utility library to write microservices in clojure."
   :url "https://github.com/zalando-stups/friboo"
 
