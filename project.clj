97707162798e0fe6f9eec3c3b8b03945a4d27f33
Andre Hartmann <Andre.Hartmann@zalando.de>
2015-08-31 11:56:37 +0200

    patched swagger1st version

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
   :java-source-paths ["java"]
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.zalando/swagger1st "0.16.1"]
+                 [org.zalando/swagger1st "0.16.2"]
                  [org.zalando.stups/txdemarcator "0.7.0"]
                  [com.stuartsierra/component "0.2.3"]
                  [ring "1.4.0"]
