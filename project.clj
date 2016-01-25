8fb661c75b931aad5b0dc7bda058137b4d4666d9
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-01-25 15:31:11 +0100

    Version 1.5.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/friboo "1.5.4-SNAPSHOT"
+(defproject org.zalando.stups/friboo "1.5.4"
   :description "A utility library to write microservices in clojure."
   :url "https://github.com/zalando-stups/friboo"
 
