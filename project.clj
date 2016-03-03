fbfedf39dea957c6f2b51c58d4bb2d4a9ad26b55
Tobias Sarnowski <tobias@sarnowski.io>
2016-03-03 14:11:15 +0100

    Version 1.8.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/friboo "1.7.0"
+(defproject org.zalando.stups/friboo "1.8.0-SNAPSHOT"
   :description "A utility library to write microservices in clojure."
   :url "https://github.com/zalando-stups/friboo"
 
