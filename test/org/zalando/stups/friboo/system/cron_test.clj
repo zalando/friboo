7865dac377008292c6a728c9e57cee1f6d167c0d
Sebastian Poeplau <sebastian.poeplau@zalando.de>
2016-10-06 14:50:05 +0200

    Fix unit test
    The Cron component is immutable, so the right approach is to thread it
    through the calls to `start` and `stop`.

--- test/org/zalando/stups/friboo/system/cron_test.clj
+++ test/org/zalando/stups/friboo/system/cron_test.clj
@@ -1,7 +1,8 @@
 (ns org.zalando.stups.friboo.system.cron-test
   (:require [org.zalando.stups.friboo.system.cron :refer :all]
             [clojure.test :refer :all]
-            [overtone.at-at :as at]))
+            [overtone.at-at :as at]
+            [com.stuartsierra.component :as component]))
 
 (def-cron-component TestCron [state]
   (at/at (at/now) (job deliver state 42) pool))
@@ -10,7 +11,8 @@
   ;; Here we make sure that the component is started and stopped properly.
   (let [state (promise)
         cron-component (map->TestCron {:state state})]
-    (.start cron-component)
-    (.stop cron-component)
-    (.stop cron-component) ; stopping twice shouldn't break anything
+    (-> cron-component
+        component/start
+        component/stop
+        component/stop) ; stopping twice shouldn't break anything
     (is (= 42 (deref state 500 :not-delivered)))))
