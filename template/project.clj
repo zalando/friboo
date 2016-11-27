008358c104b9a8996b4df55660e101b14b74b48e
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2016-11-27 16:38:42 +0100

    workarounds to fix template releasing

--- template/project.clj
+++ template/project.clj
@@ -5,6 +5,16 @@
             :url  "http://www.apache.org/licenses/"}
   :eval-in-leiningen true
   :deploy-repositories [["releases" :clojars]]
+  :plugins [[lein-shell "0.5.0"]]
+  :release-tasks [["shell" "git" "diff" "--exit-code"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+  :vcs :git
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [org.clojure/tools.namespace "0.2.11"]
                                   [org.clojure/java.classpath "0.2.3"]
