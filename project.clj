99d57816e015d13b0bc7e606cd6062657e8dc399
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2016-11-27 16:10:14 +0100

    Version 2.0.0-beta5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/friboo "2.0.0-beta5-SNAPSHOT"
+(defproject org.zalando.stups/friboo "2.0.0-beta5"
   :description "An utility library to write microservices in clojure."
   :url "https://github.com/zalando/friboo"
 
