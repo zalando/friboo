379f8012a69439731711e0cc33ea6ffe10e04bd7
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2016-11-27 13:26:31 +0100

    Add a namespace for dev/test utils

---
+++ src/org/zalando/stups/friboo/dev.clj
@@ -0,0 +1,17 @@
+(ns org.zalando.stups.friboo.dev
+  (:require [clojure.edn :as edn])
+  (:import (java.net ServerSocket)))
+
+(defn slurp-if-exists [file]
+  (when (.exists (clojure.java.io/as-file file))
+    (slurp file)))
+
+(defn load-dev-config [file]
+  (edn/read-string (slurp-if-exists file)))
+
+(defn get-free-port []
+  (let [sock (ServerSocket. 0)]
+    (try
+      (.getLocalPort sock)
+      (finally
+        (.close sock)))))
