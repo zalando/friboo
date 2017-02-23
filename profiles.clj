47f9c0d59a2e6279ac6813e371c12d66ab13efb2
Vasiliy Avdiushkin <dixelt@gmail.com>
2017-02-23 14:10:23 +0100

    make log functions called during the tests

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,6 @@
 {:test {:dependencies [[midje "1.8.3"]
-                       [org.postgresql/postgresql "9.4.1212"]]
+                       [org.postgresql/postgresql "9.4.1212"]
+                       [org.slf4j/slf4j-log4j12 "1.7.12"]]
         :env          {:tokeninfo-url "default-tokeninfo"}}
  :dev  {:repl-options {:init-ns user}
         :source-paths ["dev"]
